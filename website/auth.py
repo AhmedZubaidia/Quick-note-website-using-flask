7e447645c1c3abd94a96289d1cf7d00f9f2005a2
AhmadASR5 <ahmadragb@hotmail.com>
2024-07-17 16:39:17 +0300

    adding notes feature

--- website/auth.py
+++ website/auth.py
@@ -4,6 +4,8 @@ from . import db
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from flask_login import login_user, login_required, logout_user, current_user
+
 auth = Blueprint('auth', __name__)
 
 
@@ -20,17 +22,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in successfully", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash("Incorrect password, try again", category='error')
         else:
             flash("Email does not exist", category='error')
 
-    return render_template("login.html", text="i passed from the auth" )
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
-
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
@@ -56,8 +61,9 @@ def sign_up():
             new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash("Account created", category='success')
             return redirect(url_for('views.home'))
 
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
