763a95e49362aecf73215d0f04458d462604cdaf
AhmadASR5 <ahmadragb@hotmail.com>
2024-07-18 03:18:44 +0300

    adding animation for background

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint , render_template , request , flash , redirect , url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 from . import db
 from .models import User
@@ -9,10 +9,8 @@ from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 
-
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -31,15 +29,16 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up',methods=['GET','POST'])
-def sign_up():
 
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
@@ -58,12 +57,12 @@ def sign_up():
         elif len(password1) < 7:
             flash("Password must be at least 7 characters", category='error')
         else:
-            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(email=email, firstName=firstName,
+                            password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash("Account created", category='success')
             return redirect(url_for('views.home'))
-
 
     return render_template("sign_up.html", user=current_user)
